77f38e9136768b815e20b444e4183e6f2b46d4cc
Ivan <ivanvlora@gmail.com>
2019-03-28 21:02:35 +0900

    added an flv player to the app

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 import { Segment, Dimmer, Loader } from "semantic-ui-react";
 
 class StreamShow extends Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+
+    await this.props.fetchStream(id);
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://www.localhost:8000/live/${id}.flv`
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+    this.player.play();
   }
 
   render() {
@@ -22,6 +39,7 @@ class StreamShow extends Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
